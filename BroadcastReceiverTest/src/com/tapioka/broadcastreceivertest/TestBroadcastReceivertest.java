996055fde0f9abbf4255e9dfa6618d395659a997
pirorin91 <pirorin91@gmail.com>
2014-10-26 17:48:48 +0900

    Test with ACTION_BOOT_COMPLETED

---
+++ BroadcastReceiverTest/src/com/tapioka/broadcastreceivertest/TestBroadcastReceivertest.java
@@ -0,0 +1,23 @@
+package com.tapioka.broadcastreceivertest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class TestBroadcastReceivertest extends BroadcastReceiver {
+
+	private final String TAG = TestBroadcastReceivertest.class.getSimpleName();
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.v(TAG, "onReceive() Tapioka");
+		String action = intent.getAction();
+		if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+			Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+}
